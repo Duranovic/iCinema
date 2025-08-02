8b5131ff757d5b738f27b2992bb9c6c8d89c3259
Velid Duranovic <velidprivate@gmail.com>
2025-08-02 13:49:20 +0200

    Heading widget

---
+++ iCinema.UI/icinema_desktop/lib/widgets/heading.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+
+class Heading extends StatelessWidget {
+  final String text;
+  final TextAlign textAlign;
+  final double? fontSize;
+  final Color? color;
+
+  const Heading(this.text, {super.key, this.textAlign = TextAlign.left, this.fontSize, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: fontSize,
+        color: color,
+      ),
+    );
+  }
+}
